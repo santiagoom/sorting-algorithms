7f5ed73947bc81f54bab387f8ac3e33be0dd5f3a
catalina85 <catalina85@163.com>
2019-12-30 09:15:58 +0800

    2019-12-30 Mon 09:15:58 from robot.

---
+++ algos/cpp/QuickSort/quicksort_practise.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <myutils.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void quickSort(vector<int> &nums) {
+        quickSortUtil(nums, 0, nums.size() - 1);
+    }
+
+private:
+    void quickSortUtil(vector<int> &nums, int start, int end) {
+        if (start < end) {
+            int base = nums[start];
+            int i = start;
+            int j = end + 1;
+            while (true) {
+                while (i < end && base >= nums[++i]);
+                while (j > start && base <= nums[--j]);
+                if (i < j) {
+                    swap(nums, i, j);
+                } else {
+                    break;
+                }
+            }
+            swap(nums, start, j);
+            quickSortUtil(nums, start, j - 1);
+            quickSortUtil(nums, j + 1, end);
+        }
+    }
+
+    void swap(vector<int> &nums, int a, int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+};
+
+int main() {
+//    2019-12-29-0 10:17:48
+    auto *so = new Solution();
+    vector<int> nums{40, 70, 50, 30, 35, 80, 65, 55, 60, 45};
+    print_1d_vector(nums);
+    so->quickSort(nums);
+    print_1d_vector(nums);
+    cout << "new file!" << endl;
+    delete so;
+    return 0;
+}
