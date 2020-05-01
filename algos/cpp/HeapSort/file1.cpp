c58b580fc1e282ddf6a6b8447f0eadc229a9fc18
catalina85 <catalina85@163.com>
2020-05-01 23:02:56 +0800

    2020-05-01 Fri 23:02:56 from robot.

---
+++ algos/cpp/HeapSort/file1.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <vector>
+# include <myutils.h>
+# include <myListNode.h>
+
+using namespace std;
+
+class Solution {
+
+};
+
+int main() {
+    auto *so = new Solution();
+    cout << "new file!" << endl;
+    delete so;
+    return 0;
+}
